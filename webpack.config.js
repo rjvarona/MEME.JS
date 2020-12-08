fed99098c28fc8c080c022af444a8d43ec680763
rjvarona <rjvarona16@gmail.com>
2020-12-08 12:05:14 -0800

    added webpack cli correct versions and webpack servers

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	mode: 'development',
 	entry: './src/index.js',
@@ -6,4 +9,11 @@ module.exports = {
 		filename: './main.bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+	],
 };
