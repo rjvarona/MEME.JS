656b09993a79a83d58f01ceea5db08f0fa510c48
rjvarona <rjvarona16@gmail.com>
2020-12-08 11:04:27 -0800

    initialize webpack and node

---
+++ core.js
@@ -0,0 +1,29 @@
+//prototypal inheritance
+
+function Vector(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+Vector.prototype.toString = function () {
+	return 'x: ' + this.x + ' y: ' + this.y;
+};
+
+Vector.prototype.add = function (x, y) {
+	this.x += x;
+	this.y += y;
+};
+
+//prototypal inheritance
+// .prototype is for the pr
+function Point(x, y, colour) {
+	Vector.apply(this, arguments);
+	this.colour = colour;
+}
+
+Point.prototype = new Vector();
+
+//-------------constructor is a property that allows you to specify the function's constructor
+Point.prototype.constructor = Point;
+
+//---------------------------------------Classical Inheritance
