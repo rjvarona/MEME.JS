fe150e2c272052d383801ceac1615f8b10aa1dac
rjvarona <rjvarona16@gmail.com>
2020-12-08 14:15:13 -0800

    added to routelist

--- src/core/jsMvcFramework.js
+++ src/core/jsMvcFramework.js
@@ -1,3 +1,5 @@
+import route from 'color-convert/route';
+
 let routeList = [];
 
 export default class JsMvcFramework {
@@ -8,5 +10,15 @@ export default class JsMvcFramework {
 	//needs to change to take multiple ids later on maybe custom dictionary
 	matchRoute(route1, route2) {
 		return route1 === route ? true : false;
+	}
+
+	navigate(url) {
+		routeList.forEach(route => {
+			if (this.matchRoute(route.path, url)) {
+				//load DOM
+				//return view for html add specific html for specific path
+				document.getElementById('meme').innerHTML = route.controller().view();
+			}
+		});
 	}
 }
